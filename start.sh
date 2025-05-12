5e6768cfccbf09c2018da1c215add76fba6413c0
Mukund Babbar <mbabbar@splunk.com>
2025-05-12 10:06:30 +1000

    RUM placeholder

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Insert the AppDynamics snippet if provided
+if [[ -n "${APPD_JS_SNIPPET}" ]]; then
+  echo "Injecting AppDynamics snippet..."
+  sed -i "s|<!-- APPD_SNIPPET_PLACEHOLDER -->|${APPD_JS_SNIPPET}|" /app/templates/base.html
+fi
+
 exec python3 app.py --external-url "${ENDPOINT_URL}"
