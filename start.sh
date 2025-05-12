ac20b3bfa217b97ceeec0db7455dd30e17136906
Mukund Babbar <mbabbar@splunk.com>
2025-05-12 14:24:26 +1000

    template

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+set -e
 
-# Test
-# Insert the AppDynamics snippet if provided
-if [[ -n "${APPD_JS_SNIPPET}" ]]; then
-  echo "Injecting AppDynamics snippet..."
-  sed -i "s|<!-- APPD_SNIPPET_PLACEHOLDER -->|${APPD_JS_SNIPPET}|" /app/templates/base.html
-fi
+# Inject snippet into base.html at runtime
+sed -i "/<!-- APPD_SNIPPET_PLACEHOLDER -->/ {
+    r /app/config/snippet.js
+    d
+}" /app/templates/base.html
 
 exec python3 app.py --external-url "${ENDPOINT_URL}"
